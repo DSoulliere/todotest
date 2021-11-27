33731e93045c96c3a1c373fb7b7862f67140d65d
Derek Soulliere <derek.soulliere@gmail.com>
2021-11-27 06:04:23 -0500

    Update default seeder to create admin, dummy users, and tasks

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Models\TaskList;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Role::firstOrCreate(['name' => 'administrator']);
+
+        //Create the administrative user
+        $admin = User::firstOrNew([
+            'role_id' => 1,
+            'name' => "Admin",
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $admin->email_verified_at = now();
+
+        $admin->save();
+
+
+        TaskList::factory()->for($admin)->hasTasks(random_int(4, 20))->count(2)->create();
+
+        //Create users with default
+        User::factory()->has(
+            TaskList::factory()->hasTasks(random_int(4, 20))->count(10)
+        )->count(10)->create();
     }
 }

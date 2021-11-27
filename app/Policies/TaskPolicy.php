8fcbc3f6355f41042a8640e61c805e17d0f99621
Derek Soulliere <derek.soulliere@gmail.com>
2021-11-27 08:18:11 -0500

    Add policy for task creation

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TaskPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Perform pre-authorization checks.
+     *
+     * @param  \App\Models\User  $user
+     * @param  string  $ability
+     * @return void|bool
+     */
+    public function before(User $user)
+    {
+        if ($user->role->name === 'administrator') {
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function use(User $user, Task $task)
+    {
+        return $user->id === $task->user_id;
+    }
+}

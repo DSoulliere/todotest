6e022f2c6f76541f3d006aa8242c7f8b70895ab0
Derek Soulliere <derek.soulliere@gmail.com>
2021-11-27 08:16:22 -0500

    Add functionality to detect if user is admin & display proper component

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <div class="container mx-auto px-4 sm:px-8 max-w-full">
+                        @if (session('status'))
+                            <div class="flex-auto  px-2 py-2 bg-green-200 rounded-full font-semibold text-green-900 leading-tight text-center">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        @if($user->role->name === 'user')
+                            <x-task-list :tasks="$tasks"></x-task-list>
+                        @else
+                            <x-user-list :users="$users"></x-user-list>
+                        @endif
+                    </span>
+
                 </div>
             </div>
         </div>
